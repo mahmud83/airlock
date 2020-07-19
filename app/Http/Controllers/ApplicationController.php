dc8c0615b0760ba3292a7120f55c4f888b9b4867
Shellrean <wandinak17@gmail.com>
2020-07-19 11:27:24 +0700

    Sso apps

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -59,7 +59,7 @@ class ApplicationController extends Controller
     public function show(Application $app)
     {
         $token = auth()->user()->createToken('Personal Access Token')->accessToken;
-        return redirect($app->url.'?token='.$token);
+        return redirect($app->url.'token='.$token);
     }
 
     /**
